4ed0451aa002e7b74da27184ec47ec704c91c254
duyvu <duyvukim752000@gmail.com>
2023-10-05 03:45:05 +0700

    Finish StackByLinkedlist with enhance comment on check parenthese

--- src/stack/theories/StackByArrayList.java
+++ src/stack/theories/StackByArrayList.java
@@ -14,50 +14,51 @@ public class StackByArrayList<E> extends ArrayList implements StackADT<E> {
 
     @Override
     public boolean isEmpty() {
-	return this.size() == 0;
+        return this.size() == 0;
     }
 
     @Override
-    public void push(E e) {
-	this.add(e);
+    public E push(E e) {
+        this.add(e);
+        return e;
     }
 
     @Override
     public E pop() {
-	if (this.isEmpty()) {
-	    return null;
-	}
-	return (E) this.remove(this.size() - 1);
+        if (this.isEmpty()) {
+            return null;
+        }
+        return (E) this.remove(this.size() - 1);
     }
 
     @Override
     public E peek() {
-	if (this.isEmpty()) {
-	    return null;
-	}
-	return (E) this.get(this.size() - 1);
+        if (this.isEmpty()) {
+            return null;
+        }
+        return (E) this.get(this.size() - 1);
     }
 
     public void printAll() {
-	for (Object el : this) {
-	    System.out.println(el);
-	}
+        for (Object el : this) {
+            System.out.println(el);
+        }
     }
 
     // Main class for testing
     public static void main(String[] args) {
-	StackByArrayList<Integer> stack = new StackByArrayList<>();
-	stack.push(1);
-	stack.push(2);
-	stack.push(3);
-	stack.push(4);
-	stack.push(5);
-	stack.push(6);
-	stack.pop();
+        StackByArrayList<Integer> stack = new StackByArrayList<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
+        stack.pop();
 
-	stack.printAll();
+        stack.printAll();
 
-	System.out.println("Size: " + stack.size());
+        System.out.println("Size: " + stack.size());
     }
 
 }
--- src/stack/theories/StackByLinkedList.java
+++ src/stack/theories/StackByLinkedList.java
@@ -13,32 +13,73 @@ import list.theories.singly.SinglyTesting;
  * @author duyvu
  * @param <E>
  */
-public class StackByLinkedList<E> extends LinkedList<E> implements StackADT<E> {
+public class StackByLinkedList<E> implements StackADT<E> {
 
+    // ====================================
+    // = Fields
+    // ====================================
     // Plot the SinglyLinkedList as the core of StackADT
-//    private SinglyTesting<E> list;
+    private LinkedList<E> list;
+
+    // ====================================
+    // = Constructors
+    // ====================================
     /**
      * Default Constructor <br>
      *
      * A newly created stack based on the initial empty singly linked list
      */
     public StackByLinkedList() {
-	super();
+        list = new LinkedList<E>();
     }
 
+    // ====================================
+    // = Methods implemented from Interface
+    // ====================================
     @Override
-    public void push(E e) {
-	this.addFirst(e);
+    public E push(E e) {
+        list.addFirst(e);
+        return e;
     }
 
     @Override
     public E pop() {
-	return this.removeFirst();
+        if (isEmpty()) {
+            return null;
+        }
+        return list.removeFirst();
     }
 
     @Override
     public E peek() {
-	return this.peekFirst();
+        if (isEmpty()) {
+            return null;
+        }
+        return list.peekFirst();
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public void clear() {
+        this.clear();
+    }
+
+    // ====================================
+    // = Methods 
+    // ====================================
+    public void displayyAll() {
+        for (E el : list) {
+            System.out.println(el);
+        }
     }
 
     /**
@@ -47,76 +88,78 @@ public class StackByLinkedList<E> extends LinkedList<E> implements StackADT<E> {
      * @param html
      * @return true if all of html tags has been matched or not
      */
-    public boolean isHTMLMatched(String html) {
-	StackADT<String> buffer = new StackByLinkedList<>();
+    public static boolean isHTMLMatched(String html) {
+        // Create a stack to handle tag
+        StackADT<String> buffer = new StackByLinkedList<>();
 
-	int left = html.indexOf("<");
+        // <html> : start with <
+        // </html>: start with <
+        int left = html.indexOf("<");
 
-	while (left != -1) {
-	    int right = html.indexOf(">", left + 1); // Find the next right matching
-	    if (right == -1) {				// If does not match, return false
-		return false;
-	    }
+        while (left != -1) {
 
-	    // Extract the sub-string between <...> and push into the stack
-	    // e.g. <html> -> html
-	    String tag = html.substring(left + 1, right);
+            // Check if having closing open paren
+            int right = html.indexOf(">", left + 1); // Find the next right matching
+            if (right == -1) {				// If does not match, return false
+                return false;
+            }
 
-	    // Pusing the opening tag
-	    // e.g. <html></html>, -> push only html, not the /html from the closing tag
-	    if (!tag.startsWith("/")) {
-		buffer.push(tag);
-	    } // Checking the closing tag with opening tag in the stack
-	    else {
+            // Extract the sub-string between <...> and push into the stack
+            // e.g. <html> -> html
+            String tag = html.substring(left + 1, right);
+            if (tag == null) {
+                return false;                           // If not having tag, return false
+            }
 
-		// If the buffer is false, meanning that the opening did not detected, then return false
-		if (buffer.isEmpty()) {
-		    return false;
-		}
+            // If html -> push into the buffer
+            // If /html -> comparing with the html on top
+            if (!tag.startsWith("/")) {
+                buffer.push(tag);
+            } else {
 
-		// e.g. /html -> html, extract the html only 
-		// e.g [html, body -> pop -> body then comparing with body
-		// if not matched, then return false
-		if (!tag.substring(1).equals(buffer.pop())) {
-		    return false;
-		}
-	    }
+                // If the buffer is false, meanning that the opening did not detected, then return false
+                if (buffer.isEmpty()) {
+                    return false;
+                }
 
-	    // Find the next left matching, after the first right tag
-	    // e.g. <html>..... < -> this one
-	    left = html.indexOf("<", right + 1);
-	}
+                // e.g. /html -> html, extract the html only at the 1 st index
+                // e.g [html, body -> pop -> body then comparing with body
+                if (!tag.substring(1).equals(buffer.pop())) {
+                    return false;
+                }
+            }
 
-	return buffer.isEmpty();
-    }
+            // Find the next left matching, after the first right tag
+            // e.g. <html>..... < -> this one
+            left = html.indexOf("<", right + 1);
+        }
 
-    public void displayyAll() {
-	for (Object el : this) {
-	    System.out.println(el);
-	}
+        // return true if no tag in the buffer
+        return buffer.isEmpty();
     }
 
     // Main class for testing
     public static void main(String[] args) {
-	StackByLinkedList<Integer> stack = new StackByLinkedList<>();
+        StackByLinkedList<Integer> stack = new StackByLinkedList<>();
 
-	// Testing the stack 
-	stack.push(1);
-	stack.push(2);
-	stack.push(3);
-	stack.push(4);
-	stack.push(5);
-	stack.push(99);
+        // Testing the stack 
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.push(99);
 
-	stack.pop();
-	stack.displayyAll();
+        stack.pop();
+        stack.displayyAll();
 
-	// Testing the problem tested on the stackbylinkedlist
-	System.out.println("isHTMLTagsMatched: " + stack.isHTMLMatched("<html></html>"));
-	System.out.println("isHTMLTagsMatched: " + stack.isHTMLMatched("<html><body></html>"));
-	System.out.println("isHTMLTagsMatched: " + stack.isHTMLMatched("<html><body</html>"));
-	System.out.println("isHTMLTagsMatched: " + stack.isHTMLMatched("<html><body><body></html>"));
-	System.out.println("isHTMLTagsMatched: " + stack.isHTMLMatched("<html><body></body></html>"));
-	System.out.println("isHTMLTagsMatched: " + stack.isHTMLMatched("<html><body></></html>"));
+        // Testing the problem tested on the stackbylinkedlist
+        System.out.println("isHTMLTagsMatched: " + StackByLinkedList.isHTMLMatched("<html></html>"));
+        System.out.println("isHTMLTagsMatched: " + StackByLinkedList.isHTMLMatched("<html><body></html>"));
+        System.out.println("isHTMLTagsMatched: " + StackByLinkedList.isHTMLMatched("<html><body</html>"));
+        System.out.println("isHTMLTagsMatched: " + StackByLinkedList.isHTMLMatched("<html><body><body></html>"));
+        System.out.println("isHTMLTagsMatched: " + StackByLinkedList.isHTMLMatched("<html><body></body></html>"));
+        System.out.println("isHTMLTagsMatched: " + StackByLinkedList.isHTMLMatched("<html><body></></html>"));
     }
+
 }
