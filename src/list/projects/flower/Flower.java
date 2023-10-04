339bfc3a9001586088086c1946a29870365c4c22
duyvu <duyvukim752000@gmail.com>
2023-10-04 14:16:35 +0700

    Adding some getters & setters

--- src/list/projects/flower/Flower.java
+++ src/list/projects/flower/Flower.java
@@ -13,27 +13,83 @@ public class Flower {
     // ====================================
     // = Fields
     // ====================================
-    String name;
-    String original;
-    int price;
+    private String name;
+    private String original;
+    private int price;
 
     // ====================================
     // = Constructor
     // ====================================
-    
     /**
-     * Create constructor flower
-     * @param name 
+     * Constructor for finding flower by name
+     *
+     * @param name
      */
     public Flower(String name) {
         this.name = name;
     }
 
+    /**
+     * Constructor for loading data
+     *
+     * @param name
+     */
     public Flower(String name,
                   String original,
                   int price) {
         this.name = name;
         this.original = original;
+        this.price = price;
+    }
+
+    // ====================================
+    // = Methods
+    // ====================================
+    /**
+     * For finding Flower by name
+     *
+     * @param obj
+     * @return
+     */
+    @Override
+    public boolean equals(Object obj) {
+        return this.name.equalsIgnoreCase(((Flower) obj).getName());
+    }
+
+    /**
+     * String representation of the Flower object
+     *
+     * @return string
+     */
+    @Override
+    public String toString() {
+        return name + ", " + original + ", " + price;
+    }
+
+    // ====================================
+    // = Setters & Getters
+    // ====================================
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOriginal() {
+        return original;
+    }
+
+    public void setOriginal(String original) {
+        this.original = original;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
         this.price = price;
     }
 
