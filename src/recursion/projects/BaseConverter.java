6db2af7ccb0a1c89bc1cc3e70b63ae538f269565
duyvu <duyvukim752000@gmail.com>
2023-10-10 10:17:27 +0700

    Add and Commit 2 new files

---
+++ src/recursion/projects/BaseConverter.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package recursion.projects;
+
+/**
+ *
+ * @author duyvu
+ */
+public class BaseConverter {
+
+    public static String baseConverter(int num,
+                                       int base) {
+        if (num == 0) {
+            return "0";
+        } else {
+            return baseConverter(num / base, base) + Character.forDigit(num % base, base);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(baseConverter(266, 2));
+        System.out.println(baseConverter(266, 10));
+        System.out.println(baseConverter(266, 8));
+        System.out.println(baseConverter(266, 16));
+        System.out.println(baseConverter(266, 24));
+
+    }
+}
---
+++ src/recursion/projects/ReverseArray.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package recursion.projects;
+
+/**
+ *
+ * @author duyvu
+ */
+public class ReverseArray {
+
+    public static void reverseArray(int[] arr,
+                                    int low,
+                                    int high) {
+        if (low > high) {
+            return;
+        } else {
+            int temp = arr[low];
+            arr[low] = arr[high];
+            arr[high] = temp;
+            reverseArray(arr, low + 1, high - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] list = new int[]{1, 2, 3, 5, 6, 7, 8, 9, 19};
+        reverseArray(list, 0, list.length - 1);
+    }
+}
