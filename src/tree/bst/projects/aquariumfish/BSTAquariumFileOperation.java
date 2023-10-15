b921b0dc0bb8640b5d79252700304ffc8f72e586
duyvu <duyvukim752000@gmail.com>
2023-10-15 22:56:28 +0700

    Add File operation for Inorder

--- src/tree/bst/projects/aquariumfish/BSTAquariumFileOperation.java
+++ src/tree/bst/projects/aquariumfish/BSTAquariumFileOperation.java
@@ -5,10 +5,15 @@
 package tree.bst.projects.aquariumfish;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import tree.node.BSTNode;
 
 /**
@@ -16,8 +21,11 @@ import tree.node.BSTNode;
  * @author duyvu
  */
 public abstract class BSTAquariumFileOperation {
-    
-    public static Scanner sc = new Scanner(System.in);
+
+    private static String FILENAME_OUTPUT_BFS = ".\\src\\tree\\bst\\projects\\aquariumfish\\data\\outputBFS.txt";
+    private static String FILENAME_INORDER_OUTPUT = ".\\src\\tree\\bst\\projects\\aquariumfish\\data\\outputINORDER.txt";
+
+    private static Scanner sc = new Scanner(System.in);
 
     /**
      * Print out the node to the screen
@@ -49,34 +57,53 @@ public abstract class BSTAquariumFileOperation {
      * @param pw
      * @param root
      */
-    public static void BFS_file(PrintWriter pw,
-                                BSTNode root) {
+    private static void BFS_file(PrintWriter pw,
+                                 BSTNode root) {
+
+        // If the tree is null then return nothing
         if (root == null) {
             System.out.println("Empty tree");
             return;
         }
-        
+
         Queue<BSTNode> queue = new LinkedList();
         queue.add(root);
-        
+
         BSTNode currNode;
-        
+
         while (!queue.isEmpty()) {
 
             // Extract the node and print to the file
-            currNode = queue.poll();
-            visit_file(pw, root);
+            currNode = queue.remove();
+            visit_file(pw, currNode);
 
             // Expanding to left and right
             if (currNode.left != null) {
-                queue.add((BSTNode) root.left);
-            } else if (currNode.right != null) {
-                queue.add((BSTNode) root.right);
+                queue.add((BSTNode) currNode.left);
+            }
+
+            if (currNode.right != null) {
+                queue.add((BSTNode) currNode.right);
             }
         }
 
         // Close PrintWriter after printing the entire tree
         pw.close();
+    }
+
+    /**
+     * Wrapper file of the BFS_file function
+     *
+     * @param root
+     */
+    public static void BFS_file(BSTNode root) {
+        try (FileWriter fw = new FileWriter(new File(FILENAME_OUTPUT_BFS)); PrintWriter pw = new PrintWriter(fw);) {
+
+            BFS_file(pw, root);
+
+        } catch (IOException ex) {
+            Logger.getLogger(BSTAquariumFileOperation.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -89,10 +116,25 @@ public abstract class BSTAquariumFileOperation {
                                     BSTNode root) {
 
         // if nothing than printout the tree is empty
-        if (root != null) {            
+        if (root != null) {
             inorder_file(pw, (BSTNode) root.left);
             visit_file(pw, root);
             inorder_file(pw, (BSTNode) root.right);
-        }        
+        }
+    }
+
+    /**
+     * Inorder traversal to print the output to file
+     *
+     * @param root
+     */
+    public static void inorder_file(BSTNode root) {
+        try (FileWriter fw = new FileWriter(new File(FILENAME_INORDER_OUTPUT)); PrintWriter pw = new PrintWriter(fw);) {
+
+            inorder_file(pw, root);
+
+        } catch (IOException ex) {
+            Logger.getLogger(BSTAquariumFileOperation.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
